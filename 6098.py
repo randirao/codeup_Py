58c0b9aefb32f0d62bb416eb11405d243fb974b5
Han2 <randirao4245@gmail.com>
2024-08-31 23:17:38 +0900

    6098

--- 6098.py
+++ 6098.py
@@ -1,18 +1,26 @@
-arr = []
+# arr = [[0 for col in range(10)] for row in range(10)]
 
-for i in range(10):
-    arr.append([0] * 10)
+arr = [list(map(int, input().split())) for _ in range(10)]
 
 #입력받기   
-for i in range(10) :
-    for j in range(10) :
-        arr[i] = map(int, input().split())
+# for i in range(10) :
+#     for j in range(10) :
+#         arr[i][j] = int(input())
 
-# while True :
-arr[1][1] = 9
+x, y = 1, 1
+
+while True :
+    arr[x][y] = 9             #arr[x][y]는 9로 초기화
+    if arr[x][y] == 2 :       #먹이가 있으면
+        break                 #멈춰!
+    elif arr[x][y+1] == 1 :   #오른쪽이 막혔으면
+        x += 1                #밑으로 ㄱㄱ
+    else :                    #아니면
+        y += 1                #오른쪽으로 ㄱㄱ
+    
 
 #출력하기
 for i in range(10):
     for j in range(10):
         print(arr[i][j], end=' ')
-    print()
+    print("")
